7bf52e6b57be1f703abad04928527904310c4061
sravuri23 <sravuri23@gmail.com>
2018-03-11 10:36:12 +0000

    New feature commited to the branch

--- src/test/java/com/salmon/test/step_definitions/gui/FreeCrmSteps.java
+++ src/test/java/com/salmon/test/step_definitions/gui/FreeCrmSteps.java
@@ -1,17 +1,22 @@
 package com.salmon.test.step_definitions.gui;
 
+import com.salmon.test.framework.helpers.Props;
 import com.salmon.test.page_objects.gui.FreeCrmPage;
 import cucumber.api.DataTable;
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.assertj.core.api.Assertions;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
+import static org.junit.Assert.assertThat;
+
 public class FreeCrmSteps {
 
-    private FreeCrmPage freeCrmpage;
+private FreeCrmPage freeCrmpage;
 
 public FreeCrmSteps(FreeCrmPage freeCrmpage){
     this.freeCrmpage = freeCrmpage;
@@ -19,7 +24,7 @@ public FreeCrmSteps(FreeCrmPage freeCrmpage){
 }
 
     @When("^I click on signUp link from the home page$")
-    public void iClickOnSignUpLinkFromTheHomePage()  {
+    public void iClickOnSignUpLinkFromTheHomePage() throws InterruptedException {
 
 
     System.out.println("Link clicked");
@@ -29,30 +34,44 @@ public FreeCrmSteps(FreeCrmPage freeCrmpage){
 
     @When("^the user selects free edition from the drop down$")
     public void the_user_selects_free_edition_from_the_drop_down() {
+    freeCrmpage.selectEdition();
+
 
     }
 
-    @When("^the user fills in the registration form with all valid details$")
-    public void the_user_fills_in_the_registration_form_with_all_valid_details(DataTable arg1)  {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-        // E,K,V must be a scalar (String, Integer, Date, enum etc)
-        throw new PendingException();
+    @And("^the user fills in the registration form with all valid details$")
+    public void the_user_fills_in_the_registration_form_with_all_valid_details(){
+           freeCrmpage.enterFirstNameData();
+           freeCrmpage.enterLastNameData();
+           freeCrmpage.enterEmailAddress();
+           freeCrmpage.enterconfirmEmailAddress();
+           freeCrmpage.enterUserNameData();
+           freeCrmpage.enterPasswordData();
+           freeCrmpage.confirmPasswordData();
+
+
+
+
+
+
     }
 
     @When("^the user clicks on submit$")
-    public void the_user_clicks_on_submit() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void the_user_clicks_on_submit() {
+        freeCrmpage.clickOnAgreeCheckBox();
+        freeCrmpage.clickOnSubmitButton();
+
     }
 
     @Then("^the user successfully registers in the freecrm site$")
-    public void the_user_successfully_registers_in_the_freecrm_site() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
+    public void the_user_successfully_registers_in_the_freecrm_site() {
+        Assertions.assertThat(freeCrmpage.formConfirmationMessageisDisplayed());
 
+    }
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+    // E,K,V must be a scalar (String, Integer, Date, enum etc)
 
 
 }
--- src/test/resources/features/web/FreeCrm.feature
+++ src/test/resources/features/web/FreeCrm.feature
@@ -4,15 +4,7 @@ Feature: This feature is to practice list and Maps and getting the user name and
   Scenario: Verify the user is able to register in the site using (Model class)
     Given the user is on the "Home" page
     When I click on signUp link from the home page
-#    And the user selects free edition from the drop down
-#    And  the user fills in the registration form with all valid details
-#      | userDetails             |
-#      | Kumar                   |
-#      | Ravuri                  |
-#      | ravurikumar01@gmail.com |
-#      | ravurikumar01@gmail.com |
-#      | ravuri                  |
-#      | Kumar2323!              |
-#      | Kumar2323               |
-#    When the user clicks on submit
-#    Then the user successfully registers in the freecrm site
+    And the user selects free edition from the drop down
+    And  the user fills in the registration form with all valid details
+    When the user clicks on submit
+    Then the user successfully registers in the freecrm site
