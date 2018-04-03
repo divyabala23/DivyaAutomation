8a665416cd33ca6e581588e79b2e538844568cc7
sravuri23 <sravuri23@gmail.com>
2018-04-03 20:21:35 +0100

    Added more scenarios to weather api feature

--- src/test/java/com/salmon/test/step_definitions/api/SampleWeatherSteps.java
+++ src/test/java/com/salmon/test/step_definitions/api/SampleWeatherSteps.java
@@ -24,6 +24,11 @@ public class SampleWeatherSteps {
     private String ENDPOINT_GET_WEATHERREPORT_CITY = "http://restapi.demoqa.com/utilities/weather/city";
     private ValidatableResponse json;
     private String responseBody;
+    private String Location = "city";
+    private String contentType;
+    private String serverType;
+    private String acceptLanguage;
+
 
 
     @Given("^I requet GetWeatherDetails by location$")
@@ -47,6 +52,7 @@ public class SampleWeatherSteps {
 
         json = response.then().statusCode(statusCode);
         Assert.assertEquals(statusCode,response.getStatusCode());
+
         System.out.println("The status code displayed is : " + response.getStatusCode());
         }
 
@@ -56,6 +62,44 @@ public class SampleWeatherSteps {
         responseBody = response.getBody().asString();
         System.out.println("Response Body is => "  + responseBody);
 
+
+    }
+
+    @Then("^Verify the (\\d+) success response in the request$")
+    public void verifyTheSuccessResponseInTheRequest(int statusCode) {
+
+        response = request.get(Location);
+        statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode/*actual value*/, 200 /*expected value*/, "correct status code displayed");
+        System.out.println(" The response code returned is : " + response.getStatusCode());
+
+    }
+
+
+
+    @Then("^Verify the “HTTP (\\d+) OK” response is returned$")
+    public void verifyTheHTTPOKResponseIsReturned(String statusLine) {
+
+        statusLine = response.getStatusLine();
+        Assert.assertEquals(statusLine/*actual value */,"HTTP/1.1 200 OK" /*expected value*/,  "Correct status code returned");
+        System.out.println("The reponse code returned is "  + response.getStatusLine());
+
+
+    }
+
+    @And("^Verify the content type in the response header$")
+    public void verifyTheContentTypeInTheResponseHeader() {
+
+        contentType = response.header("Content-Type");
+        System.out.println("Content-Type value : " + contentType);
+
+
+        serverType = response.header("Server");
+        System.out.println("Server value : " + serverType);
+
+
+        acceptLanguage = response.header("Content-Encoding");
+        System.out.println("Content-Encoding: " + acceptLanguage);
 
     }
 }
--- src/test/resources/features/api/SampleWeather.feature
+++ src/test/resources/features/api/SampleWeather.feature
@@ -1,18 +1,50 @@
 @api
 
-  Feature: Weather API
+Feature: Weather API
 
-    Scenario Outline: Check the weather report from location hyderabad
+  Scenario Outline: Check the weather report from location hyderabad
 
-      Given I requet GetWeatherDetails by location
-      When  I request location by  "<city>"
-      Then  Verify the 200 response in the request
-      And   I get response with weather report
+    Given I requet GetWeatherDetails by location
+    When  I request location by  "<city>"
+    Then  Verify the 200 response in the request
+    And   I get response with weather report
 
-   Examples:
+    Examples:
 
-   | city      |
-   | Hyderabad |
-   | Mumbai    |
-   | Delhi     |
+      | city      |
+      | Hyderabad |
+      | Mumbai    |
+      | Delhi     |
 
+
+  Scenario Outline: Verify the success response code
+    Given I requet GetWeatherDetails by location
+    When  I request location by  "<city>"
+    Then  Verify the 200 success response in the request
+
+    Examples:
+
+      | city       |
+      | Vijayawada |
+
+
+  Scenario Outline: Verify the error response code
+    Given I requet GetWeatherDetails by location
+    When  I request location by  "<city>"
+    Then  Verify the 200 success response in the request
+
+    Examples:
+
+      | city        |
+      | 78789798798 |
+
+  Scenario Outline: Verify the response status line
+    Given I requet GetWeatherDetails by location
+    When  I request location by  "<city>"
+    Then  Verify the “HTTP 200 OK” response is returned
+    And   Verify the content type in the response header
+
+    Examples:
+
+      | city        |
+      | Kolkata     |
